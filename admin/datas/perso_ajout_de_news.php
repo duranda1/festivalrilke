78f2add29ef43d6ddde2429c4f316a624c79d64d
unknown <StdF@WHL00232A.(none)>
2012-01-12 10:39:51 +0100

    commit admin

--- admin/datas/perso_ajout_de_news.php
+++ admin/datas/perso_ajout_de_news.php
@@ -4,36 +4,22 @@
 	<table>
 		<tr>
 			<td>Titre</td>
-			<td>
-				<input type='text' name='titre' size='30'maxlength="50" class='text'/>
-			</td>
+			<td><input type='text' name='titre' size='30'maxlength="50" class='text'/></td>
 		</tr>
 		<tr>
 			<td>Description</td>
-			<td>
-				<input type='text' name='description' size='30'maxlength="100" class='text'/>
-			</td>
+			<td><input type='text' name='description' size='30'maxlength="100" class='text'/></td>
 		</tr>
 		<tr>
 			<td>Liens</td>
-			<td>
-				<input type='text' name='lien' size='30'maxlength="100" class='text'/>
-			</td>
+			<td><input type='text' name='lien' size='30'maxlength="100" class='text'/></td>
 		</tr>
 		<tr>
 			<td>Date</td>
-			<td>				
-				<script>DateInput('orderdate', true, 'DD-MON-YYYY')</script>
-				<!--<input type="datetime" name="date" class="text" value="01.12.2012 08:53">-->
-			</td>
+			<td><script>DateInput('orderdate', true, 'DD-MON-YYYY')</script></td>
 		</tr><tr>
-			<td>Heure</td>
-			<td>				
-				<select name='heure' class="text">
-					<option value="1">bnob</option>
-					<?php echo '<option value="1">1</option>'; ?>
-				</input>
-			</td>
+			<td>Heure <span style="font-size: 10">(hh:mm)</span></td>
+			<td><input type='text' name='heure' size='1'maxlength="5" class='text' value="00:00"/></td>
 		</tr>
 		<tr>
 			<td colspan='2' align='center'>
