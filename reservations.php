d6b9a9796b3b2a80a0ef497b9f6c865fd4a9aa2b
unknown <StdF@WHL00231A.(none)>
2012-01-12 15:51:58 +0100

    d

--- reservations.php
+++ reservations.php
@@ -9,8 +9,18 @@
 	<a href="./index.php" data-role="button"  data-theme="a"><div class="retourAuMenu">Retour au menu</div></a>
 	<div data-role="content" data-theme="a">
 		<div class="contentZone">	
-			<form method=POST action=mailReservations.php >				
-				     Email: <INPUT NAME=Nom size=30 style="background-color: #999999; size: 20%; color: #000000;">				
+			<h3><div class="reservation1">Réservation</div></h3>
+			<span class="mainTitle">
+						<span style="font-weight:bold" class="reservation2">Envoyez-nous votre adresse mail ainsi que le nombre de billets désirés</span>
+				</span>	
+			<form method=POST action=mailReservations.php">				
+				    </br>
+				     Email: 
+				     <input name=Nom size=30 style="background-color: #999999; color: #000000; width: 200px; height: 12px">	
+				    </br> 
+				    <div class="reservation3">Nombre de billets: </div>
+				    <input name=Nom size=30 style="background-color: #999999; color: #000000; width: 30px; height: 12px;">	
+				    </br>			
 				<input type="submit" value="Ok"/>			
 			</form>			
 		</div>
