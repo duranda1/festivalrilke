1f54a50a96694649338fe2dc543aa1450260ef3c
unknown <StdF@WHL00231A.(none)>
2012-01-12 14:29:30 +0100

    e

--- reservations.php
+++ reservations.php
@@ -9,6 +9,7 @@
 	<a href="./index.php" data-role="button"  data-theme="a"><div class="retourAuMenu">Retour au menu</div></a>
 	<div data-role="content" data-theme="a">
 		<div class="contentZone">	
+<<<<<<< HEAD
 			<form method=POST action=mailReservations.php >
 				<fieldset>				  				
 				  <label for="mail">Votre email :</label>
@@ -17,6 +18,19 @@
 				</fieldset>
 				<input type="submit" value="Envoyer" />
 			</form>			
+=======
+			<form method="POST" action="mailReservations.php" >
+				<input type="hidden" name="subject" value="formmail">
+				<table>
+					<tr>
+						<td>Votre Email:</td>
+						<td><input type="text" name="email" size="30" style="color: #ffffff;"></td>
+					</tr>
+				</table>
+				<br> <input type="submit" value="Envoyer"> 
+			</form>
+			<a href="./contact.php" data-role="button"><span class="bouttonEnvoi">Envoyer</span></a>
+>>>>>>> 6310eb570fa4a48fb567086508cdac35c91c1758
 		</div>
 	</div><!-- /content -->
 <?php include('footer.php'); ?>
