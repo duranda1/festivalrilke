af5f75168cf6b8787e7946432a1bd29e1e8e6ee0
unknown <StdF@WHL00232A.(none)>
2012-01-13 11:42:23 +0100

    reservation

--- mailReservations.php
+++ mailReservations.php
@@ -1,9 +1,11 @@
 <?php
-$to = "artawende@gmail.com";
+$to = "loic.haenni@students.hevs.ch";
 $from= $_POST['nom'].' '.$_POST['prenom'].' '.$_POST['email'];
 $subject ="Reservations billets";
 $message = "Reservation";
 $nb = $_POST['nb'];
+ini_set('SMTP', 'mail.hevs.ch');
+ini_set('smtp_port', '587');
 mail($to, $subject, $message, $from, $nb);
 //Header("Location:./confirmReservations.php");
 ?>
