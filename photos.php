881842233fb34be3c36c2ac648980ebaf8df9709
unknown <StdF@WHL00229A.(none)>
2012-01-13 10:15:30 +0100

    gallerie photos

--- photos.php
+++ photos.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html> 
 <html> 
 	<head> 
-	<title>Photos</title> 
+	<title>Gallerie photos</title> 
 	<?php include('init.php'); ?>
-	<?php include('medias/photos/photosHeader.php'); ?>
 </head> 
 <body> 
 <?php include('header.php'); ?>
 	<script type="text/javascript" src="cookies.js"></script>
 	<a href="./index.php" data-role="button"  data-theme="a"><div class="retourAuMenu">Retour au menu</div></a>
-	<?php include('medias/photos/photosContent.php'); ?>
+	
+	<iframe src="medias/photos/photos.html" height="500" width="100%" scrolling="no" frameborder="0"></iframe>
+	
 <?php include('footer.php'); ?>
 </div><!-- /page one -->
 </body>
