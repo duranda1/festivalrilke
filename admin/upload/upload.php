c3ce8c3102690f8ebabbb804223f423619fc336a
unknown <StdF@WHL00232A.(none)>
2012-01-16 09:23:28 +0100

    commit upload

--- admin/upload/upload.php
+++ admin/upload/upload.php
@@ -24,7 +24,29 @@
 	<script type="text/javascript" src="./js/prototype.js"></script>
     <script type="text/javascript" src="./js/scriptaculous.js"></script>
     <script type="text/javascript" src="multiupload.js"></script>
-	<body onload="init();">
+	<body>
+		<?php
+			function make_thumb($src,$dest,$desired_width, $desired_height)
+			{
+			
+			  /* read the source image */
+			  $source_image = imagecreatefromjpeg($src);
+			  $width = imagesx($source_image);
+			  $height = imagesy($source_image);
+			  
+			  /* find the "desired height" of this thumbnail, relative to the desired width  */
+			  //$desired_height = floor($height*($desired_width/$width));
+			  
+			  /* create a new, "virtual" image */
+			  $virtual_image = imagecreatetruecolor($desired_width,$desired_height);
+			  
+			  /* copy source image at a resized size */
+			  imagecopyresized($virtual_image,$source_image,0,0,0,0,$desired_width,$desired_height,$width,$height);
+			  
+			  /* create the physical thumbnail image to its destination */
+			  imagejpeg($virtual_image,$dest);
+			}
+		?>
 		<div id="header">
 			<h1>Festival Rilke</h1>
 			<h2>Administration</h2>
@@ -36,12 +58,50 @@
 		            
 		            <?php
 		            	$nom = $_POST['galerie'];
-						@mkdir ('test/'.$nom,0777,true);
-		            	$dossier = 'test/'.$nom.'/';
-					    foreach($_FILES as $fichier)
+		            	$chemin = '../../medias/photos/images/'.$nom;
+						@mkdir ($chemin,0777,true);
+		            	$dossier = $chemin.'/';
+					    foreach($_FILES as $photos)
 						{
-						    print $fichier['name'] . "<br/>";
-							move_uploaded_file($fichier['tmp_name'], $dossier . $fichier['name']);
+						    print $photos['name'] . "<br/>";
+							move_uploaded_file($photos['tmp_name'], $dossier . $photos['name']);
+							$img_src_chemin = $dossier.$photos['name'];
+							@mkdir ('../../medias/photos/thumbs/'.$nom,0777,true);
+							$img_dst_chemin = '../../medias/photos/thumbs/'.$nom.'/'.$photos['name'];
+							make_thumb($img_src_chemin, $img_dst_chemin, 75, 75);
+							$fichier = '../../medias/photos/thumbs/'.$nom.'/desc.xml';
+							$dom = new DOMDocument('test1');
+							if(file_exists($fichier)==false){
+								//touch($fichier);
+								$leFichier = fopen($fichier, "wb");
+								$contenu = '<?xml version="1.0" encoding="ISO-8859-1" ?>';
+								$contenu .= '<descriptions>';
+								$contenu .= '</descriptions>';
+								
+								fwrite($leFichier,$contenu);
+								fclose($leFichier);
+	
+							}
+					        $dom->load($fichier);
+					        /*nouvelle balise <image>*/
+					        $new_image = $dom->createElement('image');			
+					        /*nouvelle balise <name>*/			
+					        $new_name = $dom->createElement('name');			
+					        $name_content = $dom->createTextNode($photos['name']);			
+					        $lename = $new_name->appendChild($name_content);			
+					        $leimage = $new_image->appendChild($new_name);	
+							/*nouvelle balise <name>*/			
+					        $new_text = $dom->createElement('text');			
+					        $text_content = $dom->createTextNode('Nouvelle image '.$photos['name']);			
+					        $letext = $new_text->appendChild($text_content);			
+					        $leimage = $new_image->appendChild($new_text);	
+					        /*on rattache tout le <file> au DOM*/	
+					        
+					        $channel = $dom->getElementsByTagName("descriptions")->item(0);
+		  					$channel->appendChild($new_image);
+		  						
+							/*on enregistre dans un fichier*/					
+							$dom->save($fichier);
 						}
 					?>
 		        </div>
