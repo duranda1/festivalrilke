40972bde07f6bb060e4c35f2aa8d78f6f856f822
unknown <StdF@WHL00229A.(none)>
2012-01-18 14:50:15 +0100

    cookie

--- cookies.js
+++ cookies.js
@@ -1,12 +1,24 @@
 //Fonction qui écrit dans un cookie la langue sélectionnée en cliquant sur un bouton
-function setCookie(value) {
-	var dateDuJour = new Date();
-	var dateExpire = new Date();
-	
-	dateExpire.setMonth(dateExpire.getMonth()+1);
-		
-	document.cookie = "langue=" + value + ";expires=" + dateExpire.toGMTString;
+// function setCookie(value) {
+	// var dateDuJour = new Date();
+	// var dateExpire = new Date();
+// 	
+	// dateExpire.setMonth(dateExpire.getMonth()+1);
+// 	
+// 	
+	// document.cookie = "langue=" + value + ";expires=" + dateExpire.toGMTString;
+// 
+// }
+
+function setCookie(value)
+{
+	var exdays=10;
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie="langue=" + c_value;
 }
+
 
 //Fonction qui récupère la langue dans le cookie
 function getCookie() {
